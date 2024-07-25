8ec54115ec946fbae14a7cb99cab0245fcf44670
Gabriel <gabrielromanrojas@gmail.com>
2024-07-26 00:57:14 +0100

    Finished main JS validation for application form - Added CSS to validation.

--- js/external.js
+++ js/external.js
@@ -3,19 +3,75 @@ const lName = document.getElementById('lName');
 const email = document.getElementById('email');
 const phone = document.getElementById('phone');
 const fileUpload = document.getElementById('fileUpload');
-const successModal = document.getElementById('successModal');
+const successMessage = document.getElementById('successMessage');
 const applicationForm = document.getElementById('applicationForm');
 const applicationBox = document.getElementById('applicationBox');
 const applicationTitle = document.getElementById('applicationTitle');
+const fNameMissing = document.getElementById('fNameMissing');
+const lNameMissing = document.getElementById('lNameMissing');
+const emailMissing = document.getElementById('emailMissing');
+const phoneMissing = document.getElementById('phoneMissing');
+const fileUploadMissing = document.getElementById('fileUploadMissing');
+const phoneNumMissing = document.getElementById('phoneNumMissing');
 
 applicationSubmitted();
-
+fName.addEventListener('input', function() {
+    fNameMissing.style.display = 'none';
+    fName.style.borderColor = '#ddd';
+});
+lName.addEventListener('input', function() {
+    lNameMissing.style.display = 'none';
+    lName.style.borderColor = '#ddd';
+})
+email.addEventListener('input', function() {
+    emailMissing.style.display = 'none';
+    email.style.borderColor = '#ddd';
+})
+phone.addEventListener('input', function() {
+    phoneMissing.style.display = 'none';
+    phone.style.borderColor = '#ddd';
+})
+phone.addEventListener('input', function() {
+    phoneNumMissing.style.display = 'none';
+    phone.style.borderColor = '#ddd';
+})
+fileUpload.addEventListener('input', function() {
+    fileUploadMissing.style.display = 'none';
+    fileUpload.style.borderColor = '#ddd';
+})
 // Tries to submit the application when the 'submit' button is clicked.
 applicationForm.addEventListener('submit', (e) => {
     // As long as there are no errorMessages, the application form will be submitted successfully.
     let errorMessages = [];
     if (fName.value === '' || fName.value === null) {
-        errorMessages.push('Please input your name.');
+        errorMessages.push('.');
+        fNameMissing.style.display = 'block';
+        fName.style.borderColor = 'rgba(219,37,11,255)';
+    } 
+    if (lName.value === '' || lName.value === null){
+        errorMessages.push('.')
+        lNameMissing.style.display = 'block';
+        lName.style.borderColor = 'rgba(219,37,11,255)';
+    } 
+    if (email.value === '' || email.value === null){
+        errorMessages.push('.');
+        emailMissing.style.display = 'block';
+        email.style.borderColor = 'rgba(219,37,11,255)';
+    }
+    if (phone.value === '' || phone.value === null){
+        errorMessages.push('.');
+        phoneMissing.style.display = 'block';
+        phone.style.borderColor = 'rgba(219,37,11,255)';
+    }
+    if (isNaN(parseInt(phone.value, 10)) && phone.value !== '' || phone.value.toString() !== phone.value && phone.value !== '') {
+        errorMessages.push('.');
+        phoneNumMissing.style.display = 'block';
+        phone.style.borderColor = 'rgba(219,37,11,255)';
+      }
+    if (fileUpload.value === '' || fileUpload.value === null){
+        errorMessages.push('.');
+        fileUploadMissing.style.display = 'block';
+        fileUpload.style.borderColor = 'rgba(219,37,11,255)';
     }
     // If there's at least an error, the form will fail to submit.
     if (errorMessages.length > 0) {
@@ -31,7 +87,7 @@ function applicationSubmitted(){
     if (localStorage.getItem('applicationSuccess') === 'true') {
         applicationBox.style.display = 'none';
         applicationTitle.style.display = 'none';
-        successModal.style.display = 'block';
+        successMessage.style.display = 'block';
         localStorage.removeItem('applicationSuccess');
     }
 }
